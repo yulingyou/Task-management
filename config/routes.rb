ab59ead526ec96a75f4616a5a80684843637aeb1
Daisy <ysiad926@gmail.com>
2022-06-29 23:01:03 +0800

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/register', to: 'users#new'
   resources :users, only: [:create]
+  get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
+  resource :sessions, only: [:create]
 end
