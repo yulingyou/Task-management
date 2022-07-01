dce7696e756dab7462a42d3d7d8761b77a348deb
Daisy <ysiad926@gmail.com>
2022-07-02 00:06:58 +0800

    switch cookies to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if cookies[:user_id]
-      User.find(cookies.encrypted.signed[:user_id])
+    if session[:user_id]
+      User.find(session[:user_id])
     end
   end
 end
