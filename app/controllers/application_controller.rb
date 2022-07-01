ccc9456b86c0a88a5eabd2b7a618c792da335544
Daisy <ysiad926@gmail.com>
2022-07-01 23:58:06 +0800

    add cookies.signed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    if cookies[:user_id]
+      User.find(cookies.signed[:user_id])
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: user_params[:email])
 
     if @user && @user.password == user_params[:password]
-      cookies[:user_id] = @user.id
+      cookies.signed[:user_id] = @user.id
       redirect_to register_path
     else
       flash.now[:notice] = "Invalid email or password"
