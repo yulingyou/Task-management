57e7f3fe3587b326f1057e55747086149dd6f738
Daisy <ysiad926@gmail.com>
2022-07-04 23:50:36 +0800

    add user model password method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: user_params[:email])
 
-    if @user && @user.password == user_params[:password]
+    if @user && @user.is_password?(user_params[:password])
       session[:user_id] = @user.id
       redirect_to register_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def password=(raw)
+    self.password_digest = BCrypt::Password.create(raw)
+  end
+
+  def is_password?(raw)
+    BCrypt::Password.new(password_digest).is_password?(raw)
+  end
 end
